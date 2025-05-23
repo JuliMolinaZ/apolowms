e2d9409aabb2af17d086822bb5090b75931e9cc1
Julián Molina <julianmolina.ing@gmail.com>
2025-05-23 16:19:05 -0500

    Add setup script and update Dockerfiles

--- apps/backend/src/modules/users/users.module.ts
+++ apps/backend/src/modules/users/users.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService], // Esto es crucial para exportar la dependencia
